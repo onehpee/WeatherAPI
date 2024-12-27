3136571de2adc95271a46b0eb718356a7cbf32cf
Adedayo UWensuyi <onehpee@yahoo.com>
2024-12-26 22:22:41 -0600

    Temp Route and Blueprint fix.

--- app/blueprints/auth/routes.py
+++ app/blueprints/auth/routes.py
@@ -2,3 +2,15 @@ from flask import Blueprint
 
 auth_routes = Blueprint('route', __name__)
 
+@auth_routes.route('/login')
+def login():
+    return "<p>Login</p>"
+
+@auth_routes.route("/logout")
+def logout():
+    return "<p>Logout</p>"
+
+@auth_routes.route("/signup")
+def sign_up():
+    return "<p>Sign up</p>"
+
