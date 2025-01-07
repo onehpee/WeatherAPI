e979a81a70a52d1e8769581b744c2690ecf08589
Adedayo UWensuyi <onehpee@yahoo.com>
2025-01-06 22:27:29 -0600

    broken blueprint

--- app/__init__.py
+++ app/__init__.py
@@ -6,9 +6,9 @@ def create_app():
     app.config['SECRET_KEY'] = 'chama'
     
     from .routes import approutes
-    #from blueprints.auth.routes import auth_routes
+    from .auth import auth
     
     app.register_blueprint(approutes, url_prefix='/')
-    #app.register_blueprint(auth_routes, url_prefix='/')
+    #app.register_blueprint(auth, url_prefix='/')
     
     return app
--- app/blueprints/auth/__init__.py
+++ app/blueprints/auth/__init__.py
@@ -1,14 +1,14 @@
-from flask import Flask
+# from flask import Flask
 
-def create_app():
-    app = Flask(__name__)
-    #encrypt and secure cookies and session data
-    app.config['SECRET_KEY'] = 'chama'
+# def create_app():
+#     app = Flask(__name__)
+#     #encrypt and secure cookies and session data
+#     app.config['SECRET_KEY'] = 'chama'
     
-    from app.routes import approutes
-    from .routes import auth_routes
+#     from app.routes import approutes
+#     from .routes import auth_routes
     
-    app.register_blueprint(approutes, url_prefix='/')
-    app.register_blueprint(auth_routes, url_prefix='/')
+#     app.register_blueprint(approutes, url_prefix='/')
+#     app.register_blueprint(auth_routes, url_prefix='/')
     
-    return app
+#     return app
