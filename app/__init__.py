8df53cfe3bd186694b780f1ec1cadaaca3d05453
Adedayo UWensuyi <onehpee@yahoo.com>
2025-01-17 23:13:41 -0600

    combine app and api

--- app/__init__.py
+++ app/__init__.py
@@ -1,15 +1,31 @@
 from flask import Flask
-from flask_restx import Api
+from flask_restful import Resource, Api
+from flask_sqlalchemy import SQLAlchemy
 
-def create_app():
-    app = Flask(__name__, template_folder='blueprints/auth/templates/auth')
-    #encrypt and secure cookies and sessions data
-    app.config['SECRET_KEY'] = 'chama'
+
+app = Flask(__name__)
+api = Api(app)
+
+Data = []
+
+class User(Resource):
+    # def getUser():
+        
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
+# def create_app():
+#     app = Flask(__name__, template_folder='blueprints/auth/templates/auth')
+#     app = Api(app)
+#     #encrypt and secure cookies and sessions data
+#     app.config['SECRET_KEY'] = 'chama'
     
-    from .routes import approutes
-    from .blueprints .auth .routes import auth_routes
+#     from .routes import approutes
+#     from .blueprints .auth .routes import auth_routes
     
-    app.register_blueprint(approutes, url_prefix='/')
-    app.register_blueprint(auth_routes, url_prefix= '/')
+#     app.register_blueprint(approutes, url_prefix='/')
+#     app.register_blueprint(auth_routes, url_prefix= '/')
     
-    return app
+#     return app
+
