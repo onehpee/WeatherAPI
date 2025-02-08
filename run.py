043a11ae24d1b77d1abc274502e755ad845687a8
Adedayo UWensuyi <onehpee@yahoo.com>
2025-02-07 23:09:14 -0600

    successful responses, now formatting responses

--- run.py
+++ run.py
@@ -1,6 +1,15 @@
-from app import create_app
+#from app import create_app
 
-webapp = create_app()
+#webapp = create_app()
+
+from app import main as get_weather
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
 
 if __name__ == '__main__':
-    webapp.run(debug=True)
+    app.run(debug=True)
