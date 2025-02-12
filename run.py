eea5625b6e305233ccdaec07ef8b14b3d8c40559
Adedayo UWensuyi <onehpee@yahoo.com>
2025-02-11 23:10:26 -0600

    forms, html, posting, request

--- run.py
+++ run.py
@@ -3,12 +3,18 @@
 #webapp = create_app()
 
 from app import main as get_weather
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+import requests
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/', method=['GET','POST'])
 def index():
+    if request.method == 'POST':
+        city = request.form['cityName']
+        state = request.form['stateName']
+        country = request.form['countryName']
+        print(get_weather(city, state, country))
     return render_template('index.html')
 
 if __name__ == '__main__':
